7d7ba14373d187b0e09f68215efd100e6fd62ff7
SerGrynn <63641478+SerGrynn@users.noreply.github.com>
2024-05-01 12:53:51 +0200

    Typo fixed + comments and btw it's almost 3hours, it's a progress !

--- Algorithm/Eyliria's_Challenge/a_dog_walk.py
+++ Algorithm/Eyliria's_Challenge/a_dog_walk.py
@@ -3,15 +3,16 @@ def a_dog_walk(dog_walk):
     East_West = 0
     if len(dog_walk) != 10:
         return False
-    for i in range(0, 9):
-        if dog_walk == 'n':
+    for i in range(0, 10):  # Fix range to include the 10th element
+        if dog_walk[i] == 'n':
             North_South += 1
-        elif dog_walk == 's':
+        elif dog_walk[i] == 's':
             North_South -= 1
-        if dog_walk == 'e':
+        elif dog_walk[i] == 'e':
             East_West += 1
-        elif dog_walk == 'w':
-            East_West = - 1
+        elif dog_walk[i] == 'w':
+            East_West -= 1  # Fix typo, should be subtraction, not assignment
     if North_South == 0 and East_West == 0:
         return True
+    return False
 print(a_dog_walk(['n', 'n', 'n', 'n', 'n', 's', 's', 's', 's', 's']))
